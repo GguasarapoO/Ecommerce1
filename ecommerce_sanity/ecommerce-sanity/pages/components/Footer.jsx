2ed32bb174b48905083db0574dd28a472c9ef122
GgusarapoO <losergusarapo@gmail.com>
2023-04-15 07:04:47 -0400

    Add Navbar & Footer

--- ecommerce_sanity/ecommerce-sanity/pages/components/Footer.jsx
+++ ecommerce_sanity/ecommerce-sanity/pages/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2023 Todos los derechos reservados</p>
+      <p>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
--- ecommerce_sanity/ecommerce-sanity/pages/index.js
+++ ecommerce_sanity/ecommerce-sanity/pages/index.js
@@ -7,8 +7,8 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
+      <h2>Mejores ventas</h2>
+      <p>Consolas y accesorios variados</p>
     </div>
 
     <div className="products-container">
